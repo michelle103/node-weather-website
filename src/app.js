b77246ff56096b362681152c4666ea9a528c9e2d
michelle103 <michelle_schott@gmx.de>
2021-10-12 09:16:40 +0200

    Modified about page

--- src/app.js
+++ src/app.js
@@ -1,104 +1,104 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Weather',
-    name: 'Andrew Mead',
-  })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Andrew Mead",
+  });
+});
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Andrew Mead',
-  })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Michelle Schott",
+  });
+});
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    title: 'Help',
-    helpText: 'This is a helpful text.',
-    name: 'Andrew Mead',
-  })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    helpText: "This is a helpful text.",
+    name: "Michelle Schott",
+  });
+});
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: 'You must provide an address!',
-    })
+      error: "You must provide an address!",
+    });
   }
 
   geocode(
     req.query.address,
     (error, { latitude, longitude, location } = {}) => {
       if (error) {
-        return res.send({ error })
+        return res.send({ error });
       }
 
       forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-          return res.send({ error })
+          return res.send({ error });
         }
 
         res.send({
           forecast: forecastData,
           location,
           address: req.query.address,
-        })
-      })
+        });
+      });
     }
-  )
-})
+  );
+});
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term!',
-    })
+      error: "You must provide a search term!",
+    });
   }
 
   res.send({
     products: [],
-  })
-})
+  });
+});
 
-app.get('/help/*', (req, res) => {
-  res.render('404', {
-    title: '404',
-    errorMsg: 'Help article not found',
-    name: 'Andrew Mead',
-  })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMsg: "Help article not found",
+    name: "Michelle Schott",
+  });
+});
 
-app.get('*', (req, res) => {
-  res.render('404', {
-    title: '404',
-    errorMsg: 'Page not found',
-    name: 'Andrew Mead',
-  })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMsg: "Page not found",
+    name: "Michelle Schott",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}.`)
-})
+  console.log(`Server is up on port ${port}.`);
+});
